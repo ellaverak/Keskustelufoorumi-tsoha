075a27211896f7086fc9c142e920b28494306c50
ellaverak <ella.korkeaaho@gmail.com>
2021-04-09 21:16:00 +0300

    Add the option to register

--- routes.py
+++ routes.py
@@ -1,7 +1,19 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect
+import users
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/register", methods=["GET","POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.register(username,password):
+            return redirect("/")
+        else:
+            return render_template("register_error.html",message="Rekisteröinti ei onnistunut")
